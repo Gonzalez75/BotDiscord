33aed63d9c236b19c434cfab90bb9ff41c71a85e
Gonzalez75 <guilherme75gonzalez@gmail.com>
2023-02-17 23:03:59 -0300

    Milior add

---
+++ commands/milior.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("Milior")
+    .setDescription("Faz o Robozão falar que é o milior"),
+
+  async execute(interaction) {
+    await interaction.reply("Milior!!!");
+  },
+};
